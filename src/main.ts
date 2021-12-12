bd88313dcce43ae36e20f2d9151a623e02baaa3c
Titouan Pellerin <titouan.pellerin@gmail.com>
2021-12-12 22:50:10 +0100

    added hello world

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import '@styles/style.scss';
+
+import { HelloWorld } from './ts/HelloWorld';
+
+const helloWorld = new HelloWorld();
+console.log(helloWorld.getStr());
+
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  const h1 = document.createElement('h1');
+  h1.textContent = helloWorld.getStr();
+  document.querySelector('.app')?.append(h1);
+}
---
+++ src/ts/HelloWorld.ts
@@ -0,0 +1,10 @@
+export class HelloWorld {
+  str: string;
+  constructor() {
+    this.str = 'Hello World !';
+  }
+
+  getStr() {
+    return this.str;
+  }
+}
